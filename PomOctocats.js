c0c61e37b20a869c1ba69a003b894e33d07f5ef4
¯\_(ツ)_/¯ <8mpomykala@stpatrick.org>
2016-10-07 10:23:26 -0500

    Add files via upload
    Made octocats slap each other using code.

---
+++ PomOctocats.js
@@ -0,0 +1,14 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+function Octocats(w,h){
+  this.weight;
+  this.height;
+
+  this.slap=function();
+  for(){
+      console.log();
+    }
+  var mattcat = new octocat ("Pomykala",8);
+  var batcat = new octocat ("bat",3);
+  var spidercat = new octocat ("Spider",1);
+  
